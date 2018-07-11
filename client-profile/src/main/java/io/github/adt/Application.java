148c7b0c63dba6c16d0330fe491918edcab727c2
Huy Trong Nguyen <huynguyen8505@gmail.com>
2018-07-11 10:10:33 +0700

    #5 Implement Config service with Spring Cloud Config

--- client-profile/src/main/java/io/github/adt/Application.java
+++ client-profile/src/main/java/io/github/adt/Application.java
@@ -4,10 +4,10 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// @EnableEurekaServer
+@EnableDiscoveryClient
 public class Application {
   @PostConstruct
   void started() {
