48809d6ffd7c6acc22d424c359c32feeaa278bfc
Huy Trong Nguyen <huynguyen8505@gmail.com>
2018-07-13 13:54:20 +0700

    #5 Implement Config service with Spring Cloud Config

--- config/src/main/java/io/github/adt/Application.java
+++ config/src/main/java/io/github/adt/Application.java
@@ -4,12 +4,10 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class Application {
   @PostConstruct
   void started() {
